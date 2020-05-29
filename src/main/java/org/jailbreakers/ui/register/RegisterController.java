68c0e5a793ab3aac4f7b7ee0721fc431c73250cf
Trupendo <jankocis02@gmail.com>
2020-05-29 19:42:04 +0200

    back button working
    (cherry picked from commit e6e90967c5ed8b4666cfa7c03c54edc44f1ddd70)

--- src/main/java/org/jailbreakers/ui/register/RegisterController.java
+++ src/main/java/org/jailbreakers/ui/register/RegisterController.java
@@ -6,7 +6,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import org.jailbreakers.obj.Layout;
+import org.jailbreakers.obj.StageHandler;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,12 +24,21 @@ public class RegisterController implements Initializable {
     private JFXPasswordField confirmPasswordField;
     @FXML
     private ImageView loadingGif;
+    @FXML
+    private Button backButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         RegisterViewModel viewModel = new RegisterViewModel();
         loadingGif.setVisible(false);
         registerButton.setOnAction(event -> viewModel.register(usernameField.getText(), passwordField.getText(), confirmPasswordField.getText()));
+        backButton.setOnAction(event -> {
+            try {
+                StageHandler.getInstance().setScene(Layout.LOGIN);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         viewModel.registerErrorProperty().addListener((observable, oldValue, newValue) -> {
             System.out.println(newValue);
