572fb2c86c1a944b233f3ab52892add421c38e81
Trupendo <jankocis02@gmail.com>
2020-05-29 20:30:07 +0200

    MainController template just few IDs actually

--- src/main/java/org/jailbreakers/ui/main/MainController.java
+++ src/main/java/org/jailbreakers/ui/main/MainController.java
@@ -1,4 +1,25 @@
 package org.jailbreakers.ui.main;
 
-public class MainController {
+import com.jfoenix.controls.JFXTextArea;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    private Label dayLabel;
+
+    @FXML
+    private Label monthLabel;
+
+    @FXML
+    private JFXTextArea notesTextArea;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
